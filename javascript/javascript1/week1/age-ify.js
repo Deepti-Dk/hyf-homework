34ad31531559b6bd2323d328951aa8d445ad3bd2
Deepti <deepti.sharma@gmail.com>
2020-05-12 19:05:50 +0200

    A future age calculator

---
+++ javascript/javascript1/week1/age-ify.js
@@ -0,0 +1,7 @@
+// Calculating age by future year
+
+let yearOfBirth = 1980;
+let yearFuture = 2035;
+let age = yearFuture - yearOfBirth;
+
+console.log('You will be ' + age + ' years old in ' + yearFuture);
