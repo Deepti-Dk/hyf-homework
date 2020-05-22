47a23f52226feef9aff7d77692c69e331cf17a83
Deepti <deepti.sharma@gmail.com>
2020-05-22 17:31:34 +0200

    feedback implemented

--- javascript/javascript1/week2/flight-booking.js
+++ javascript/javascript1/week2/flight-booking.js
@@ -1,21 +1,20 @@
+// CODEPEN check: https://codepen.io/Dee_Sharma/pen/VwvgVLz
+
 //global variable declaration
-let fullName1, fullName2;
 let useFormalName = true;
 
 //creating function to store and return formal or informal name
-function getFullName(firstName, surName, useFormalName = 'false') {
-  let fullName;
+function getFullName(firstName, surName, useFormalName = false) {
   if (useFormalName === true) {
-    fullName = 'Lord ' + firstName + ' ' + surName;
+    return 'Lord ' + firstName + ' ' + surName;
   } else {
-    fullName = firstName + ' ' + surName;
+    return firstName + ' ' + surName;
   }
-  return fullName;
 }
 //calling function for the formal full name
-fullName1 = getFullName('John', 'Smith', true);
+const fullName1 = getFullName('John', 'Smith', true);
 //calling function for the informal full name
-fullName2 = getFullName('Jane', 'Smith');
+const fullName2 = getFullName('Jane', 'Smith');
 //displaying the formal name
 console.log('First name for the booking: ' + fullName1);
 //displaying the informal name
