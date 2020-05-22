4904490e3eba682c99dfbf21609654bc35108b24
Deepti <deepti.sharma@gmail.com>
2020-05-22 17:32:11 +0200

    feedback implemented

--- javascript/javascript1/week2/weather-wear.js
+++ javascript/javascript1/week2/weather-wear.js
@@ -1,14 +1,11 @@
 //creating function to return attire for the right temperature
-function weatherWear(temp) {
-  let attire;
-  if (temp <= 10) {
-    attire = 'Time for sweaters, jackets and warm wear.';
-  } else if (temp > 10 && temp <= 20) {
-    attire = 'Enjoy summer clothes but always keep an extra layer handy.';
-  } else {
-    attire = 'T-shirts, shorts and head to the beach.';
+function weatherWear(temperature) {
+  if (temperature <= 10) {
+    return 'Time for sweaters, jackets and warm wear.';
+  } else if (temperature > 10 && temperature <= 20) {
+    return 'Enjoy summer clothes but always keep an extra layer handy.';
   }
-  return attire;
+  return 'T-shirts, shorts and head to the beach.';
 }
 
 const clothesToWear = weatherWear(25); //calling function to display attire for 25degrees
