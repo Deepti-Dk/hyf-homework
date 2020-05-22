95e7dede03862d506eed1c68bdb45749f6bb36cf
Deepti <deepti.sharma@gmail.com>
2020-05-22 17:31:49 +0200

    feedback implemented

--- javascript/javascript1/week2/student-manager.js
+++ javascript/javascript1/week2/student-manager.js
@@ -1,40 +1,39 @@
+//CODEPEN: https://codepen.io/Dee_Sharma/pen/zYveQQx
+
 //global variable declaration
 const class07Students = [];
-let index = 0;
+
 //function to add student to the array and check for class size, duplicate name check and the queen name
 function addStudentToClass(studentName) {
-  let count;
-  if (index < 6 || studentName === 'Margrethe') {
-    for (count = 0; count < index; count++) {
-      if (class07Students[count] === studentName) {
-        console.log('Student ' + studentName + ' is already in the class');
-        return;
-      }
-    }
-    class07Students.push(studentName);
-    index++;
-  } else {
-    console.log('Cannot add more students to class 07');
+  if (studentName.trim() === '') {
+    return;
   }
+  if (class07Students.length >= 6 && studentName.toLowerCase() !== 'queen') {
+    console.log('Cannot add more students to class 07');
+    return;
+  }
+  if (class07Students.includes(studentName)) {
+    console.log('Student ' + studentName + ' is already in the class');
+    return;
+  }
+  class07Students.push(studentName);
 }
 
 //function to calculate number of students in the class07
 function getNumberOfStudents() {
-  let num;
-  for (num = 0; num < index; num++) {}
-  return num;
+  return class07Students.length;
 }
 
 //test data
-addStudentToClass('John');
+addStudentToClass('Jack');
 addStudentToClass('Martha');
 addStudentToClass('Sara');
 addStudentToClass('Sara'); // wont get added as Sara is already being added
-addStudentToClass('Joackim');
-addStudentToClass('Sonia');
 addStudentToClass('Mads');
+addStudentToClass('Zade');
 addStudentToClass('Stine'); //wont get added as class size is already 6
-addStudentToClass('Margrethe'); // will get added anyways as she has queen's name
+addStudentToClass('Queen'); // will get added anyways as she has queen's name
+addStudentToClass('Grace');
 
 //displaying total number of students
 console.log('Number of students in class 07 are: ' + getNumberOfStudents());
