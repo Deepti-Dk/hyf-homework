dfc029563c0c24386cba126c26b89194d5a7ea4e
Deepti <deepti.sharma@gmail.com>
2020-05-31 09:59:09 +0200

    Feedback Implemented

--- javascript/javascript1/week3/travel-time-calculation.js
+++ javascript/javascript1/week3/travel-time-calculation.js
@@ -3,16 +3,20 @@ const travelInformation = {
   destinationDistance: 432,
 };
 
-function timeCalculate(info) {
+function getTime(info) {
   return info.destinationDistance / info.speed; //formula: time = distance / speed
 }
 
-const travelTime = timeCalculate(travelInformation);
-let hours = Math.floor(travelTime);
-let minutes = Math.ceil((travelTime % Math.floor(travelTime)) * 100);
-//if minutes calculated are greater or equal to 60, has to be converted to an hour and update remaining minutes
-if (minutes >= 60) {
-  hours += Math.floor(minutes / 60);
-  minutes -= 60;
+function formatTime() {
+  const travelTime = getTime(travelInformation);
+  let hours = Math.floor(travelTime);
+  let minutes = Math.ceil((travelTime % hours) * 60);
+  //if minutes calculated are greater or equal to 60, has to be converted to an hour and update remaining minutes
+  if (minutes >= 60) {
+    hours += Math.floor(minutes / 60);
+    minutes -= 60;
+  }
+  console.log(hours + ' hours and ' + minutes + ' minutes'); // 4 hours and 42 minutes
 }
-console.log(hours + ' hours and ' + minutes + ' minutes'); // 4 hours and 42 minutes
+
+formatTime();
