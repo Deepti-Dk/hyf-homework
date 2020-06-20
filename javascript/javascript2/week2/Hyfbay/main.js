aa648a13b23049da803873134df4bb7187cb4cf1
Deepti <deepti.sharma@gmail.com>
2020-06-21 00:05:13 +0200

    somewhat working!

--- javascript/javascript2/week2/Hyfbay/main.js
+++ javascript/javascript2/week2/Hyfbay/main.js
@@ -1,13 +1,9 @@
 console.log('Script loaded');
-
 const products = getAvailableProducts();
-const source = document.getElementById('input-text');
-//const result = document.querySelector('ul');
 
-//console.log(products);
+/**********************JS2 - W1 HOMEWORK******************* */
 /*function renderProducts(products) {
   const ul = document.querySelector('ul');
-
   products.forEach((product) => {
     const li = document.createElement('li');
     li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
@@ -15,15 +11,60 @@ const source = document.getElementById('input-text');
   });
 }
 
-
-/** JS-W2 HW************************************* */
+/************************* JS - W2 HOMEWORK *************************** */
+const source = document.getElementById('input-text');
 const ul = document.querySelector('ul');
-let filterProduct = [];
-function wordSearch(e) {
+
+function quickSearch() {
+  const filterSearchProduct = document.getElementById('input-text').value;
+
+  products.filter((product) => {
+    if (
+      product.name.includes(filterSearchProduct.toLowerCase()) &&
+      filterSearchProduct != ''
+    ) {
+      const li = document.createElement('li');
+      li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
+      ul.appendChild(li);
+    }
+  });
+}
+
+/****************************************************************** 
+TRIAL AND ERROR CODE BELOW...!*************************************
+*******************************************************************/
+/*function quickSearch() {
+  const words = source.value.split(' ');
+  console.log(words);
+  //const wordsLowerCase = words.toLowerCase();
+
+  const matchingProducts = products.filter((product) => {
+    if (product.name)
+    //console.log(product + '     in filter');
+    products.forEach((product) => {
+      product.name = product.name.toLowerCase();
+      // console.log(product.name + '            in foreach');
+    });
+    product.name.includes(words);
+  });
+
+  console.log(matchingProducts);
+  matchingProducts.forEach((product) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;
+    ul.appendChild(li);
+  });
+}
+
+/*document.getElementById(
+        'show-product'
+      ).innerHTML = `<h2>${product.name}</h2><span>price: ${product.price}</span><br><span>Rating: ${product.rating}</span>`;*/
+
+//source.addEventListener('input', wordSearch);
+
+/*function wordSearch(e) {
   console.log(e.target.value);
   filterProduct = products.filter((product) => {
-    console.log(product.name);
-    //return product.name.toLowerCase().(e.target.value.toLowerCase());
     return (
       product.name.toLowerCase().indexOf(e.target.value.toLowerCase()) >= 0
     );
@@ -37,4 +78,22 @@ function wordSearch(e) {
     ul.appendChild(li);
   });
 }
-source.addEventListener('input', wordSearch);
+let filterProduct = [];
+function quickSearch(e) {
+  filterProduct = products.find(
+    (product) =>
+      product.name.toLowerCase().includes(e.target.value.toLowerCase()) >= 0
+  );
+
+  //if (e.target.value) {
+  const li = document.createElement('li');
+  //if (product.name === word)
+  li.innerHTML = `<h2>${filterProduct.name}</h2><span>price: ${filterProduct.price}</span><br><span>Rating: ${filterProduct.rating}</span>`;
+  ul.appendChild(li);
+  /* } else {
+    ul.innerHTML = ' ';
+  }
+
+  //console.log(filterProduct)
+}
+source.addEventListener('keyup', quickSearch);*/
