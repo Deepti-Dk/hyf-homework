27174582e22348d7f09c555e0ece084309402440
Deepti <deepti.sharma@gmail.com>
2020-06-22 14:25:01 +0200

    feedback implemented

--- javascript/javascript2/week2/number-double.js
+++ javascript/javascript2/week2/number-double.js
@@ -1,14 +1,7 @@
 let numbers = [1, 2, 3, 4];
 let newNumbers = [];
-let swapArray = [];
 
-numbers.forEach((value, index) => {
-  console.log(value + '   ' + index);
-  value % 2 !== 0 ? (swapArray[index] = value * 2) : false;
-});
-
-newNumbers = swapArray.filter((value) => {
-  return value != null;
-});
-
-console.log('The doubled numbers are', newNumbers); // [2, 6]
+newNumbers = numbers
+  .filter((check) => check % 2 != 0)
+  .map((check) => check * 2);
+console.log(newNumbers);
