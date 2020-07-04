5b1bf7df7bb39d5ee7a1af5de0d03225e8b11bc7
Deepti <deepti.sharma@gmail.com>
2020-07-04 22:46:00 +0200

    feedback implemented

--- javascript/javascript3/week1/giphy-searcher/main.js
+++ javascript/javascript3/week1/giphy-searcher/main.js
@@ -8,18 +8,30 @@ const displayImages = () => {
   const numbergif = document.getElementById('number').value;
   imagelist.innerHTML = '';
 
-  fetch(
-    `http://api.giphy.com/v1/gifs/search?q=${searchgif}&api_key=${key}&limit=${numbergif}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      data.data.forEach((img) => {
-        const image = document.createElement('img');
-        image.setAttribute('src', img.images.downsized_large.url);
-        imagelist.appendChild(image);
+  if (
+    searchgif == '' ||
+    searchgif == undefined ||
+    searchgif == null ||
+    numbergif == '' ||
+    numbergif == undefined ||
+    numbergif == null
+  ) {
+    alert(
+      'Please make sure to enter SEARCH FOR GIFs \n AND \n the NUMBER OF GIFs to be displayed'
+    );
+  } else {
+    fetch(
+      `http://api.giphy.com/v1/gifs/search?q=${searchgif}&api_key=${key}&limit=${numbergif}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data.data.forEach((img) => {
+          const image = document.createElement('img');
+          image.setAttribute('src', img.images.downsized_large.url);
+          imagelist.appendChild(image);
+        });
       });
-    });
+  }
 };
-
 inputClick.addEventListener('click', displayImages);
--- javascript/javascript3/week1/weather-app/main.js
+++ javascript/javascript3/week1/weather-app/main.js
@@ -1,12 +1,10 @@
 const locationInput = document.getElementById('cityName');
 const inputClick = document.getElementById('inputClick');
+const cityName = document.getElementById('cityHeading');
 
-const details = document.querySelector('section');
-const city = document.getElementById('cityHeading');
-
-const ul1 = document.getElementById('ul1');
-const ul2 = document.getElementById('ul2');
-const ul3 = document.getElementById('ul3');
+const temperatureList = document.getElementById('ul1');
+const humidWindSpeedList = document.getElementById('ul2');
+const sunsetSunriseList = document.getElementById('ul3');
 
 const a = document.querySelector('a');
 
@@ -18,7 +16,7 @@ const getData = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        city.innerHTML = data.name;
+        cityName.innerHTML = data.name;
 
         const img = document.getElementById('icon');
         img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
@@ -28,30 +26,30 @@ const getData = () => {
 
         let li = document.createElement('li');
         li.innerHTML = 'Temp: ' + (data.main.temp - 273).toFixed(2) + '&#8451';
-        ul1.appendChild(li);
+        temperatureList.appendChild(li);
 
         li = document.createElement('li');
         li.innerHTML =
           'Feels like: ' + (data.main.feels_like - 273).toFixed(2) + '&#8451';
-        ul1.appendChild(li);
+        temperatureList.appendChild(li);
 
         li = document.createElement('li');
         li.innerHTML = 'Humidity: ' + data.main.humidity + '%';
-        ul2.appendChild(li);
+        humidWindSpeedList.appendChild(li);
         li = document.createElement('li');
         li.innerHTML = 'Wind speed: ' + data.wind.speed + ' m/s';
-        ul2.appendChild(li);
+        humidWindSpeedList.appendChild(li);
 
         li = document.createElement('li');
         let myTime = new Date(data.sys.sunrise * 1000);
         li.innerHTML =
           'Sunrise: ' + myTime.getHours() + ':' + myTime.getMinutes();
-        ul3.appendChild(li);
+        sunsetSunriseList.appendChild(li);
         li = document.createElement('li');
         myTime = new Date(data.sys.sunset * 1000);
         li.innerHTML =
           'Sunset: ' + myTime.getHours() + ':' + myTime.getMinutes();
-        ul3.appendChild(li);
+        sunsetSunriseList.appendChild(li);
         a.href = `https://www.openstreetmap.org/#map=18/${data.coord.lat}/${data.coord.lon}`;
         a.textContent = 'Click here for the location map';
       });
