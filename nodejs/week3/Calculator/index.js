cac99cc0ee0f6e4483a518be856377b7d8af5a21
Deepti <deepti.sharma@gmail.com>
2020-09-10 22:05:02 +0200

    nodejs W3 HW

---
+++ nodejs/week3/Calculator/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Getting data through query parameters using GET
+app.get('/calculator/add', async (request, response) => {
+  let result =
+    Number(request.query.firstParam) + Number(request.query.secondParam);
+  response.send(`the result is ${result}`);
+});
+app.get('/calculator/substract', async (request, response) => {
+  let result =
+    Number(request.query.firstParam) - Number(request.query.secondParam);
+  response.send(`the result is ${result}`);
+});
+app.get('/calculator/multiply', async (request, response) => {
+  let result =
+    Number(request.query.firstParam) * Number(request.query.secondParam);
+  response.send(`the result is ${result}`);
+});
+app.get('/calculator/divide', async (request, response) => {
+  let result =
+    Number(request.query.firstParam) / Number(request.query.secondParam);
+  response.send(`the result is ${result}`);
+});
+
+// Getting data through the request body using POST
+const addition = async ({ firstParam, secondParam }) => {
+  return await (firstParam + secondParam);
+};
+app.post('/calculator/add', async (request, response) => {
+  addition({
+    firstParam: Number(request.query.firstParam),
+    secondParam: Number(request.query.secondParam),
+  })
+    .then((result) => response.json(result))
+    .catch((error) => {
+      response.status(400).send('Bad request').end();
+      console.log(error);
+    });
+});
+
+const substration = async ({ firstParam, secondParam }) => {
+  return await (firstParam - secondParam);
+};
+app.post('/calculator/substract', async (request, response) => {
+  substration({
+    firstParam: Number(request.query.firstParam),
+    secondParam: Number(request.query.secondParam),
+  })
+    .then((result) => response.json(result))
+    .catch((error) => {
+      response.status(400).send('Bad request').end();
+      console.log(error);
+    });
+});
+
+const multiplication = async ({ firstParam, secondParam }) => {
+  return await (firstParam * secondParam);
+};
+app.post('/calculator/multiply', async (request, response) => {
+  multiplication({
+    firstParam: Number(request.query.firstParam),
+    secondParam: Number(request.query.secondParam),
+  })
+    .then((result) => response.json(result))
+    .catch((error) => {
+      response.status(400).send('Bad request').end();
+      console.log(error);
+    });
+});
+
+const division = async ({ firstParam, secondParam }) => {
+  return await (firstParam / secondParam);
+};
+app.post('/calculator/divide', async (request, response) => {
+  division({
+    firstParam: Number(request.query.firstParam),
+    secondParam: Number(request.query.secondParam),
+  })
+    .then((result) => response.json(result))
+    .catch((error) => {
+      response.status(400).send('Bad request').end();
+      console.log(error);
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
