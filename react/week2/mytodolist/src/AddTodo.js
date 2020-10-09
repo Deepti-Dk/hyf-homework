5c6ad26e45056e6b2d05d8ad9d178e8f30f7c4d3
Deepti <deepti.sharma@gmail.com>
2020-10-09 20:06:37 +0200

    deleteTodo working

--- react/week2/mytodolist/src/AddTodo.js
+++ react/week2/mytodolist/src/AddTodo.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+//prop-parameter for component(which is a func)
+
 const AddToDo = () => {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([
@@ -22,11 +24,18 @@ const AddToDo = () => {
   ]);
 
   const addToDo = () => {
-    const newId = Math.max(...todos.map((item) => item.id)) + 1;
+    let newId = Math.max(...todos.map((item) => item.id)) + 1;
+    if (todos.length === 0) {
+      newId = 0;
+    }
     setTodos([...todos, { id: newId, description: inputValue, status: true }]); //parameter spread
+
     setInputValue('');
   };
 
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
+  };
   return (
     <div>
       <input
@@ -43,7 +52,13 @@ const AddToDo = () => {
           <li key={item.id}>
             <span id="List-item">{item.description}</span>
             <input id="checkbox" type="checkbox" />
-            <button id="Delete-button" type="button">
+            <button
+              id="Delete-button"
+              type="button"
+              onClick={() => {
+                deleteTodo(item.id);
+              }}
+            >
               Delete
             </button>
           </li>
