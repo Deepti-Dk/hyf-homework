ceb001d8a821c8ca1eb3074ea0dd7974a2a325a6
Deepti <deepti.sharma@gmail.com>
2020-10-09 19:38:03 +0200

    addToDo working

--- react/week2/mytodolist/src/AddTodo.js
+++ react/week2/mytodolist/src/AddTodo.js
@@ -1,80 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const todos = [
-  {
-    id: 1,
-    description: 'Get out of bed',
-    status: true,
-  },
-  {
-    id: 2,
-    description: 'Brush teeth',
-    status: true,
-  },
-  {
-    id: 3,
-    description: 'Eat breakfast',
-    status: true,
-  },
-];
-
-function addTask() {
-  console.log('item added');
-}
-function handleClick() {
-  todos.push();
-}
-const AddToDoButton = () => {
-  const sayDefault = () => alert('Add a todo');
-  const checkClick = InputToDo || sayDefault;
-  return (
-    <button type="button" onClick={checkClick}>
-      Add a task
-    </button>
-  );
-};
-
-const InputToDo = () => {
-  const sayDefault = () => alert('Add a todo');
-  const checkInput = () =>
-    document.querySelector('input').value !== ''
-      ? sayDefault
-      : document.querySelector('input').value;
-  // return <input type="text" onChange={checkInput} />;
-  // <label>
-  //   <input type="text" />
-  //   {/* value={value} onChange={onChangeInput} */}
-  // </label>
-};
-
 const AddToDo = () => {
-  const stateArray = useState('default');
-  const state = stateArray[0];
-  const setState = stateArray[1];
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      description: 'Get out of bed',
+      status: true,
+    },
+    {
+      id: 2,
+      description: 'Brush teeth',
+      status: true,
+    },
+    {
+      id: 3,
+      description: 'Eat breakfast',
+      status: true,
+    },
+  ]);
+
+  const addToDo = () => {
+    setTodos([...todos, { id: 4, description: inputValue, status: true }]);
+    setInputValue('');
+  };
 
   return (
     <div>
-      {/* <InputToDo /> */}
       <input
         type="text"
         onChange={(event) => {
-          console.log(event.target.value);
+          setInputValue(event.target.value);
         }}
-        value={state}
+        value={inputValue}
       />
-      <AddToDoButton />
+      <button onClick={addToDo}>Add ToDo</button>
+
       <ol>
         {todos.map((item) => (
-          <div>
-            <li id="List-item" key={item.id}>
-              {item.description}
-            </li>
+          <li key={item.id}>
+            <span id="List-item">{item.description}</span>
             <input id="checkbox" type="checkbox" />
             <button id="Delete-button" type="button">
               Delete
             </button>
-          </div>
+          </li>
         ))}
       </ol>
     </div>
